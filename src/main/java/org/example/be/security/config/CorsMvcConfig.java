72801deac868b13ec8fc4bb224508ca5ac90ef62
JeongBuBu <zhfl1234@email.com>
2024-12-19 15:59:30 +0900

    모든 작업

--- src/main/java/org/example/be/security/config/CorsMvcConfig.java
+++ src/main/java/org/example/be/security/config/CorsMvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.oauthjwt.config;
+package org.example.be.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
