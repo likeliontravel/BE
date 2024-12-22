d316e88e47364a49a843c0748799896c6634aaf8
ANMNYG <dkslasdud@gmail.com>
2024-12-22 22:10:02 +0900

    [feat] JWT Provider 추가
    JWT Filter에서 필요한 로직인 유저 객체 정보 가져온는 로직 구현

---
+++ src/main/java/org/example/be/jwt/provider/JWTProvider.java
@@ -0,0 +1,23 @@
+package org.example.be.jwt.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.be.security.service.CustomUserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JWTProvider {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    //유저 인증객체 생성
+    public Authentication getUserDetails(String username) {
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+}
