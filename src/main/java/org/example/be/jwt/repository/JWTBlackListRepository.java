80832bc8a82b27264e8ef18afd0519607c02af4b
ANMNYG <dkslasdud@gmail.com>
2025-01-07 03:12:03 +0900

    [feat] JWT 블랙리스트 Repository 추가

--- src/main/java/org/example/be/jwt/repository/JWTBlackListRepository.java
+++ src/main/java/org/example/be/jwt/repository/JWTBlackListRepository.java
@@ -4,8 +4,11 @@ import org.example.be.jwt.domain.JWTBlackListToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface JWTBlackListRepository extends JpaRepository<JWTBlackListToken, Integer> {
 
+    Iterable<JWTBlackListToken> findAllByEmail(String email);
 
+    JWTBlackListToken findByAccessToken(String token);
 }
