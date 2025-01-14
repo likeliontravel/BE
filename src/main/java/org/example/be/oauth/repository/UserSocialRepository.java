7cbc99cebfdefd1a759f26aae643cb9d0f66c2be
JeongBuBu <zhfl1234@email.com>
2025-01-14 15:10:37 +0900

    소셜 리포지토리 name 기반 조회 추가

--- src/main/java/org/example/be/oauth/repository/UserSocialRepository.java
+++ src/main/java/org/example/be/oauth/repository/UserSocialRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserSocialRepository extends JpaRepository<SocialUserEntity, Long> {
 
+    // 기존 username 기반 조회
     SocialUserEntity findByUsername(String username);
+
+    // 추가: 사용자 이름(name) 기반 조회
+    SocialUserEntity findByName(String name);
 }
