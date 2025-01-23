736303df21fcfcd8f4ca2f6dc74b5211aaed62c6
JeongBuBu <zhfl1234@email.com>
2025-01-23 18:10:43 +0900

    자체로그인 회원 프로필 조회

--- src/main/java/org/example/be/generaluser/controller/GeneralUserController.java
+++ src/main/java/org/example/be/generaluser/controller/GeneralUserController.java
@@ -2,11 +2,14 @@ package org.example.be.generaluser.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.be.oauth.dto.CustomOAuth2User;
 import org.example.be.response.CommonResponse;
 import org.example.be.generaluser.dto.GeneralUserDTO;
 import org.example.be.generaluser.service.GeneralUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
@@ -35,20 +38,31 @@ public class GeneralUserController {
 
             generalUserService.updateGeneralUser(generalUserDTO);
 
-            return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(null,"회원 수정 성공"));
+            return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(null, "회원 수정 성공"));
 
         } catch (NoSuchElementException e) {
 
             // 회원을 찾을 수 없는 경우
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(CommonResponse.error(404,e.getMessage()));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(CommonResponse.error(404, e.getMessage()));
 
         } catch (IllegalArgumentException e) {
 
             // 요청 데이터가 없는 경우
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CommonResponse.error(400,e.getMessage()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CommonResponse.error(400, e.getMessage()));
         }
     }
 
+    // 회원 프로필 조회
+    @GetMapping("/profile")
+    public ResponseEntity<CommonResponse<GeneralUserDTO>> getProfile(Authentication authentication) {
+        // 현재 로그인된 사용자 이메일 가져오기
+        String email = authentication.getName();
+
+        // 회원 정보를 조회
+        GeneralUserDTO userProfile = generalUserService.getProfile(email);
+
+        return ResponseEntity.ok(CommonResponse.success(userProfile, "회원 프로필 조회 성공"));
+    }
 //    // 회원 탈퇴
 //    @DeleteMapping("/delete")
 //    public ResponseEntity<CommonResponse<String>> deleteUser(@RequestBody GeneralUserDTO generalUserDTO) {
