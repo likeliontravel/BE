d10010ee9d486574a78a09c4100e2151ce747ac8
JeongBuBu <zhfl1234@email.com>
2025-02-05 22:52:57 +0900

    패스워드 삭제

--- src/main/java/org/example/be/unifieduser/dto/UnifiedUserProfileDTO.java
+++ src/main/java/org/example/be/unifieduser/dto/UnifiedUserProfileDTO.java
@@ -11,6 +11,5 @@ public class UnifiedUserProfileDTO {
     private Boolean subscribed;     // 구독 여부만 포함
     private String additionalInfo; // 소셜 유저인지 일반 유저인지 구분
     private String provider;       // 소셜 제공자 이름 (예: Google, Kakao 등)
-    private String password;        // 비밀번호 추가
 
 }
