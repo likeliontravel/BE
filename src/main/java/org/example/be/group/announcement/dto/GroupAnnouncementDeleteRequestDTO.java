c2ec8085564b66648f66c17a7a3145cd320bc2be
jjuchan <yjc7241@naver.com>
2026-01-08 22:45:34 +0900

    derectory: monorepo

--- src/main/java/org/example/be/group/announcement/dto/GroupAnnouncementDeleteRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package org.example.be.group.announcement.dto;
-
-import lombok.Getter;
-
-@Getter
-public class GroupAnnouncementDeleteRequestDTO {
-    private Long id;
-    private String groupName;
-}
--- src/main/java/org/example/be/place/entity/PlaceType.java
+++
@@ -1,7 +0,0 @@
-package org.example.be.place.entity;
-
-public enum PlaceType {
-    TouristSpot,
-    Restaurant,
-    Accommodation
-}
