4a757496bb254ce9eb625b153d076c1250a56d54
jjuchan <yjc7241@naver.com>
2026-01-26 20:02:28 +0900

    feat: member domain add 및 엔티티 설계

--- backend/src/main/java/org/example/be/BeApplication.java
+++ backend/src/main/java/org/example/be/BeApplication.java
@@ -1,21 +1,20 @@
 package org.example.be;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BeApplication.class, args);
+	}
 
-//    @Bean   // 빈으로 등록된 모든 컨트롤러 엔드포인트 로그에서 보기
-//    public ApplicationRunner printMappings(RequestMappingHandlerMapping mapping) {
-//        return args -> mapping.getHandlerMethods()
-//                .forEach((info, method) -> System.out.println(info + " -> " + method));
-//    }
+	//    @Bean   // 빈으로 등록된 모든 컨트롤러 엔드포인트 로그에서 보기
+	//    public ApplicationRunner printMappings(RequestMappingHandlerMapping mapping) {
+	//        return args -> mapping.getHandlerMethods()
+	//                .forEach((info, method) -> System.out.println(info + " -> " + method));
+	//    }
 }
---
+++ backend/src/main/java/org/example/be/member/type/MemberRole.java
@@ -0,0 +1,6 @@
+package org.example.be.member.type;
+
+public enum MemberRole {
+	USER,
+	ADMIN
+}
