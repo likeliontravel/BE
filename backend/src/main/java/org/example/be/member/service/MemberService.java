1a42f143a62e6247c244f76903b7be6972fa9fb0
jjuchan <yjc7241@naver.com>
2026-01-26 20:25:43 +0900

    feat: MemberService, MemberRepository 추가

---
+++ backend/src/main/java/org/example/be/member/service/MemberService.java
@@ -0,0 +1,19 @@
+package org.example.be.member.service;
+
+import org.example.be.member.dto.MemberJoinReqBody;
+import org.example.be.member.entity.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+	public Member join(MemberJoinReqBody reqBody) {
+		if (memberRepository.existsByEmail(reqBody.email())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 가입된 이메일입니다.");
+		}
+	}
+}
