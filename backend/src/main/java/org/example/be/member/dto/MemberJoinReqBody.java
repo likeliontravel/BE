a10abf3c9994d51380693fe49e9911d526bbcb46
jjuchan <yjc7241@naver.com>
2026-01-26 20:08:20 +0900

    feat: Join, login dto 추가

--- backend/src/main/java/org/example/be/member/dto/MemberJoinReqBody.java
+++ backend/src/main/java/org/example/be/member/dto/MemberJoinReqBody.java
@@ -4,13 +4,16 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record MemberLoginReqBody(
+public record MemberJoinReqBody(
+	@NotBlank
+	String name,
+
 	@NotBlank
 	@Email
 	String email,
 
 	@NotBlank
-	@Size(min = 8)
+	@Size(min = 8, max = 16)
 	String password
 ) {
 }
