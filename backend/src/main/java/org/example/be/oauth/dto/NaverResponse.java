ae386db9991cacf80e565a773da5cbd5a9ab65e8
jjuchan <yjc7241@naver.com>
2026-02-03 16:03:44 +0900

    config: naver redirecturl 변수화

--- backend/src/main/java/org/example/be/oauth/dto/NaverResponse.java
+++ backend/src/main/java/org/example/be/oauth/dto/NaverResponse.java
@@ -2,51 +2,50 @@ package org.example.be.oauth.dto;
 
 import java.util.Map;
 
-public class NaverResponse implements OAuth2Response{
+public class NaverResponse implements OAuth2Response {
+	private final Map<String, Object> attribute;
 
-    private final Map<String, Object> attribute;
+	// 네이버는 응답 JSON 형태에 사용자 데이터가 response라는 이름의 키 값으로 존재.
+	public NaverResponse(Map<String, Object> attribute) {
+		this.attribute = (Map<String, Object>)attribute.get("response");
+	}
 
-    // 네이버는 응답 JSON 형태에 사용자 데이터가 response라는 이름의 키 값으로 존재.
-    public NaverResponse(Map<String, Object> attribute) {
-        this.attribute = (Map<String, Object>) attribute.get("response");
-    }
+	@Override
+	public String getProvider() {
+		return "naver";
+	}
 
-    @Override
-    public String getProvider() {
-        return "naver";
-    }
+	@Override
+	public String getProviderId() {
+		Object providerId = attribute.get("id");
+		if (providerId == null) {
+			throw new IllegalArgumentException("NaverLogin - ProviderId is Missing");
+		}
+		return providerId.toString();
+	}
 
-    @Override
-    public String getProviderId() {
-        Object providerId = attribute.get("id");
-        if (providerId == null) {
-            throw new IllegalArgumentException("NaverLogin - ProviderId is Missing");
-        }
-        return providerId.toString();
-    }
+	@Override
+	public String getEmail() {
+		Object email = attribute.get("email");
+		if (email == null) {
+			throw new IllegalArgumentException("NaverLogin - email is missing");
+		}
+		return email.toString();
+	}
 
-    @Override
-    public String getEmail() {
-        Object email = attribute.get("email");
-        if (email == null) {
-            throw new IllegalArgumentException("NaverLogin - email is missing");
-        }
-        return email.toString();
-    }
+	@Override
+	public String getName() {
+		Object name = attribute.get("name");
+		if (name == null) {
+			throw new IllegalArgumentException("NaverLogin - name is missing");
+		}
+		return name.toString();
+	}
 
-    @Override
-    public String getName() {
-        Object name = attribute.get("name");
-        if (name == null) {
-            throw new IllegalArgumentException("NaverLogin - name is missing");
-        }
-        return name.toString();
-    }
-
-    @Override
-    public String getProfileImage() {
-        Object profileImage = attribute.get("profile_image");
-        return profileImage != null ? profileImage.toString() : null;
-    }
+	@Override
+	public String getProfileImage() {
+		Object profileImage = attribute.get("profile_image");
+		return profileImage != null ? profileImage.toString() : null;
+	}
 
 }
