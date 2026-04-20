d75277013e11f59d9c9d1c5d5f22bf163fcd9da9
jjuchan <yjc7241@naver.com>
2026-04-20 15:58:38 +0900

    feat: chat에서 그룹 멤버 인지 검증 하기 위한 exist메서드 추가

--- backend/src/main/java/org/example/be/domain/group/repository/GroupRepository.java
+++ backend/src/main/java/org/example/be/domain/group/repository/GroupRepository.java
@@ -17,6 +17,8 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
 
 	List<Group> findByMembersContaining(Member user);
 
+	boolean existsByGroupNameAndMembers_Id(String groupName, Long memberId);
+
 	// ( 위 findByGroupName은 기본 LAZY 로딩 전략이라 서비스단 트랜잭션 밖에서 그룹 정보를 호출하면
 	// 실제 DB 쿼리가 나가지 않고 프록시 객체에만 접근할 수 있기 때문에 밖에서 사용할 수 있는 쿼리를 따로 판겁니다.)
 	// 채팅 ChannelInterceptor, 그룹 상세조회에서 사용할 fetch join 쿼리
