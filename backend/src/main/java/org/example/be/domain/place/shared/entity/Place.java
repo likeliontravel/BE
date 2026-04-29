323a92fb2103e3e282d1137773ca263a7a20f4ad
jjuchan <yjc7241@naver.com>
2026-04-29 15:47:33 +0900

    refactor: place 추상 클래스 base 상속

--- backend/src/main/java/org/example/be/domain/place/shared/entity/Place.java
+++ backend/src/main/java/org/example/be/domain/place/shared/entity/Place.java
@@ -1,5 +1,7 @@
 package org.example.be.domain.place.shared.entity;
 
+import org.example.be.global.entity.Base;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
@@ -14,7 +16,7 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 @MappedSuperclass
-public abstract class Place {
+public abstract class Place extends Base {
 
 	@Column(name = "content_id", unique = true, nullable = false)
 	private String contentId;   // Tour API 콘텐츠 ID (고유값)
@@ -39,6 +41,4 @@ public abstract class Place {
 	private Integer mLevel;     // 맵 확대 레벨 (nullable) : 지도에 마커로 표시할 때 초기 지도 줌 수준을 자동 조정하는데 사용된다고 합니다.
 
 	private String tel;         // 전화번호
-	private String createdTime; // 등록일
-	private String modifiedTime;// 수정일
 }
