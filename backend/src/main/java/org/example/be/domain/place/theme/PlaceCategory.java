03c2234bd527023bee2855aa2202621de125a423
jjuchan <yjc7241@naver.com>
2026-05-01 16:55:12 +0900

    refactor: PlaceCategory 엔티티 setter 제거 및 보호레벨 설정

--- backend/src/main/java/org/example/be/domain/place/theme/PlaceCategory.java
+++ backend/src/main/java/org/example/be/domain/place/theme/PlaceCategory.java
@@ -1,12 +1,20 @@
 package org.example.be.domain.place.theme;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 @Table(name = "place_category")
