b969a8340725c0f920b8e9c5277eccf785e7d409
jjuchan <yjc7241@naver.com>
2026-05-04 15:05:01 +0900

    fix: PlaceSearchReqBody 파라미터 바인딩 에러 수정 (int -> Integer)

--- backend/src/main/java/org/example/be/domain/place/shared/dto/PlaceSearchReqBody.java
+++ backend/src/main/java/org/example/be/domain/place/shared/dto/PlaceSearchReqBody.java
@@ -9,14 +9,14 @@ public record PlaceSearchReqBody(
 	List<String> themes,    // ex) ["체험 및 액티비티", "미식 여행 및 먹방 중심"]
 	String keyword,         // 검색어
 
-	int page, // 기본값 1
-	int size, // 기본값 30
+	Integer page, // 기본값 1
+	Integer size, // 기본값 30
 	PlaceSortType sortType // 기본값 - 이름 오름차순
 ) {
 	public PlaceSearchReqBody {
-		if (page < 1)
+		if (page == null || page < 1)
 			page = 1;
-		if (size < 1)
+		if (size == null || size < 1)
 			size = 30;
 		if (sortType == null)
 			sortType = PlaceSortType.TITLE_ASC;
