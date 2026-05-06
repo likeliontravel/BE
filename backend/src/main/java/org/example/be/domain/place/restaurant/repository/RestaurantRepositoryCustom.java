923d742aeba8691951668ac08ce8be82f1513961
jjuchan <yjc7241@naver.com>
2026-05-06 18:47:46 +0900

    refactor: 식당 레포지토리 인터페이스 반환 타입 변경 (List -> Page)

--- backend/src/main/java/org/example/be/domain/place/restaurant/repository/RestaurantRepositoryCustom.java
+++ backend/src/main/java/org/example/be/domain/place/restaurant/repository/RestaurantRepositoryCustom.java
@@ -1,12 +1,13 @@
 package org.example.be.domain.place.restaurant.repository;
 
-import org.example.be.domain.place.restaurant.entity.Restaurant;
-import org.springframework.data.domain.Pageable;
-
 import java.util.List;
 
+import org.example.be.domain.place.restaurant.entity.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface RestaurantRepositoryCustom {
-	List<Restaurant> findAllByFilters(List<String> regions, List<String> themes, String keyword, Pageable pageable);
+	Page<Restaurant> findAllByFilters(List<String> regions, List<String> themes, String keyword, Pageable pageable);
 
 	//    List<Restaurant> findByFilters(List<String> regions, List<String> themes, String keyword, Pageable pageable);
 }
