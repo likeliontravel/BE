6ebcfa791bdae418a128e4631fe3afc754f10534
jjuchan <yjc7241@naver.com>
2026-07-28 20:26:51 +0900

    feat: 그룹 창설자인지 확인하는 메서드 추가

--- backend/src/main/java/org/example/be/domain/group/repository/GroupRepository.java
+++ backend/src/main/java/org/example/be/domain/group/repository/GroupRepository.java
@@ -19,6 +19,8 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
 
 	boolean existsByGroupNameAndMembers_Id(String groupName, Long memberId);
 
+	List<Group> findAllByCreatedBy(Member member);
+
 	// ( 위 findByGroupName은 기본 LAZY 로딩 전략이라 서비스단 트랜잭션 밖에서 그룹 정보를 호출하면
 	// 실제 DB 쿼리가 나가지 않고 프록시 객체에만 접근할 수 있기 때문에 밖에서 사용할 수 있는 쿼리를 따로 판겁니다.)
 	// 채팅 ChannelInterceptor, 그룹 상세조회에서 사용할 fetch join 쿼리
