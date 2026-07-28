63191800711d0f5fc1c197fee09bfa21cd46acdc
jjuchan <yjc7241@naver.com>
2026-07-28 20:23:21 +0900

    feat: 탈퇴 시 작성한 게시글도 삭제

--- backend/src/main/java/org/example/be/domain/board/repository/BoardRepository.java
+++ backend/src/main/java/org/example/be/domain/board/repository/BoardRepository.java
@@ -1,10 +1,17 @@
 package org.example.be.domain.board.repository;
 
 import org.example.be.domain.board.entity.Board;
+import org.example.be.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
 
+	@Modifying
+	@Query("DELETE FROM Board b WHERE b.writer = :writer")
+	void deleteByWriter(@Param("writer") Member writer);
 }
