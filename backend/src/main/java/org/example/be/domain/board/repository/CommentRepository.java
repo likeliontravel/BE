d12c668096bad4b469937279e3da8f24a99326de
jjuchan <yjc7241@naver.com>
2026-07-28 20:24:57 +0900

    feat: 탈퇴 시 작성한 모든 게시글의 댓글도 삭제

--- backend/src/main/java/org/example/be/domain/board/repository/CommentRepository.java
+++ backend/src/main/java/org/example/be/domain/board/repository/CommentRepository.java
@@ -1,9 +1,18 @@
 package org.example.be.domain.board.repository;
 
 import org.example.be.domain.board.entity.Comment;
+import org.example.be.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
+
+	@Modifying
+	@Query("DELETE FROM Comment c WHERE c.writer = :writer")
+	void deleteByWriter(@Param("writer") Member writer);
+
 }
